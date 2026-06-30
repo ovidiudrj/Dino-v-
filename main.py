04276ed0e8a42d5157ba619da7da8ca74c7d2878
Dirjan Valentin Ovidiu <darjanovidiu64@gmail.com>
2026-06-30 12:42:13 +0300

    2 game states, refined collision logic, added the score
    I now have 2 game states, playing & game over.
    Playing is the default state for now and game over is triggered when a collision happens.
    The collision is now handled by the colliderect() method.
    The score is incremented when it reaches the left side of the window.
    When a collision happens the score is set to 0.

--- main.py
+++ main.py
@@ -3,7 +3,6 @@ pygame.init()
 
 screen = pygame.display.set_mode((1280, 720))
 pygame.display.set_caption("Dino(v)")
-
 clock = pygame.time.Clock()
 running = True
 
@@ -17,34 +16,68 @@ dino = pygame.image.load("dino.png").convert_alpha()
 dino = pygame.transform.scale(dino, (135, 145))
 
 pygame.mixer.music.load("collisionSound.wav")
+font = pygame.font.Font("PressStart2P.ttf", 20)
 
 cactus_x = 1280
-cactus_speed = 5
+cactus_speed = 7
+
+score = 0
+Game_over = False
+
+GameState= "playing"
 
 while running:
-    for event in pygame.event.get():
-        if event.type == pygame.QUIT:
-            running = False
-        if event.type == pygame.KEYDOWN:
-            if event.key == pygame.K_SPACE and not jumping:
-                velocity_y = -17
-                jumping = True
+    if GameState == "playing":
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                running = False
+            if event.type == pygame.KEYDOWN:
+                if event.key == pygame.K_SPACE and not jumping:
+                    velocity_y = -18
+                    jumping = True
 
-    cactus_x -= cactus_speed
-    if cactus_x < 0 :
-        cactus_x = 1280
-    velocity_y += 0.8
-    y += velocity_y
-    if y > 400 :
-        y = 400
-        jumping = False
-    if cactus_x - 50 == x + 135 and y == 400:
-        pygame.mixer.music.play()
-        print("Faaahh")
-    screen.fill((255, 255, 255))
-    cactus = pygame.draw.rect(screen , (0 , 0 , 0), ( cactus_x , 487, 50 ,50))
-    screen.blit(dino, (x, y))
-    pygame.display.flip()
-    clock.tick(60)
+        cactus_x -= cactus_speed
+
+        text = font.render("Score: " + str(int(score)), True, (0, 0, 0))
+
+        if cactus_x < 0:
+            cactus_x = 1280
+            score += 1
+
+        velocity_y += 0.8
+        y += velocity_y
+
+        if y > 400:
+            jumping = False
+            y = 400
+
+        screen.fill((255, 255, 255))
+        screen.blit(text, (15, 15))
+        dino_rect = pygame.Rect(x, y, 135, 145)
+        cactus = pygame.draw.rect(screen, (0, 0, 0), (cactus_x, 487, 50, 50))
+
+        if dino_rect.colliderect(cactus):
+            pygame.mixer.music.play()
+            Game_over = True
+            score = 0
+            GameState = "game over"
+            print("Game Over")
+            cactus_x = 1280
+
+        screen.blit(dino, (x, y))
+        pygame.display.flip()
+        clock.tick(60)
+
+    if GameState == "game over":
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                running = False
+            if event.type ==pygame.KEYDOWN:
+                if event.key == pygame.K_r:
+                    GameState = "playing"
+        text = font.render("Game Over, press R to restart", True, (0, 0, 0))
+        screen.blit(text, (380, 340 ))
+        pygame.display.flip()
+        clock.tick(60)
 
 
